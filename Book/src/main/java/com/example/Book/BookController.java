2caafc98b3ebfd446627ce44f8eadd426d00c57f
bushrajav <bushra_azhar21@yahoo.com>
2023-03-28 11:02:48 +0100

    adding search book by title functionality at backend

--- Book/src/main/java/com/example/Book/BookController.java
+++ Book/src/main/java/com/example/Book/BookController.java
@@ -3,7 +3,10 @@ package com.example.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -11,10 +14,16 @@ public class BookController {
 
     @Autowired
     BookRepository repository;
+
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/books")
     public Iterable<Book> getBooks() {
         return repository.findAll();
+    }
+
+    @GetMapping("/search")
+    public List<Book> getSearch(@RequestParam String title) {
+        return repository.findByTitle(title);
     }
 
 }
