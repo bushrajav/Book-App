3cb3a3d075192fda9e464cb71b19e292c2e5d865
bushrajav <bushra_azhar21@yahoo.com>
2023-03-28 10:41:42 +0100

    adding more data

--- Book/src/main/java/com/example/Book/BookController.java
+++ Book/src/main/java/com/example/Book/BookController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyRESTController {
+public class BookController {
 
     @Autowired
     BookRepository repository;
